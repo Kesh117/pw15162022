54353c6659f3dfcd0e6e56c4353b6dcda77d446a
CCDM08 <ccdm08@CCDM08.local>
2022-11-03 14:49:25 -0700

    PrimerJavaScript parte 2

--- PrimerJavaScript/PrimerJavaScript.js
+++ PrimerJavaScript/PrimerJavaScript.js
@@ -8,8 +8,19 @@ function QuitarSITEC(){
         alert("SITEC Borrado...")
         let chi = document.getElementById("Sitec")
         chi.innerHTML = "Shiiiiaaaaale";
+        console.log("Eston no se ve en la pagina, pero chale xd");
     }
 }
+
+async function TraePersona(){
+    const respuesta = await fetch("https://randomuser.me/api/");
+    // console.log(respuesta);
+    const datos = await respuesta.json();
+    // console.log(datos)
+    console.log(datos.results[0].name.last)
+
+}
+
 
 // alert("Mi Primer JavaScript");
 /* VARIAS LINEAS DE COMENTARIOS */
